601cbbef5c4ede6e26c011ded66d9f05275ddb5e
EL-SHERIEF <entkal@outlook.com>
2025-04-20 17:25:22 +0200

    d

--- lexend/components/blog/Newsletter.jsx
+++ lexend/components/blog/Newsletter.jsx
@@ -1,5 +1,6 @@
 "use client";
-import Image from "next/image";
+import bg from "@/public/assets/images/landing/hero-bg-2.webp";
+
 export default function Newsletter() {
   return (
     <div
@@ -8,21 +9,31 @@ export default function Newsletter() {
     >
       <div className="section-outer panel pb-4 lg:pb-6 xl:pb-9">
         <div className="container max-w-xl">
-          <div className="section-inner panel p-3 py-6 lg:p-6 xl:p-8 rounded-2 bg-secondary dark:bg-gray-800 overflow-hidden">
+          <div className="section-inner panel p-3 py-6 lg:p-6 xl:p-8 rounded-2 border-1 border-black border overflow-hidden shadow-sm text-white"
+                    style={{
+                        borderRadius: '20px',
+                        backgroundImage: `url(${bg.src})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'start center',
+                        backgroundRepeat: 'no-repeat',
+                      }}>
             <div
-              className="row child-cols-12 md:child-cols g-6 justify-between items-center"
+              className="row child-cols-12 md:child-cols g-6 justify-center items-center"
               data-uc-grid=""
             >
-              <div>
-                <div className="vstack gap-2 max-w-500px xl:max-w-600px">
-                  <h2 className="h4 md:h3 lg:h2 m-0">Get the latest updates</h2>
-                  <p className="fs-6 lg:fs-5">
+                <div className="vstack items-center gap-2 max-w-500px xl:max-w-600px">
+                <span className="fw-medium d-flex align-items-center px-2 py-1 bg-white bg-opacity-10 rounded-pill blur gap-1 m-0" style={{border:'solid 1px rgba(255, 255, 255, 0.38)'}}>
+                                   <svg width={20} height={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#000000"><g strokeWidth="0"></g><g  strokeLinecap="round" strokeLinejoin="round"></g><g><g> <path d="M421.073,221.719c-0.578,11.719-9.469,26.188-23.797,40.094v183.25c-0.016,4.719-1.875,8.719-5.016,11.844 c-3.156,3.063-7.25,4.875-12.063,4.906H81.558c-4.781-0.031-8.891-1.844-12.047-4.906c-3.141-3.125-4.984-7.125-5-11.844V152.219 c0.016-4.703,1.859-8.719,5-11.844c3.156-3.063,7.266-4.875,12.047-4.906h158.609c12.828-16.844,27.781-34.094,44.719-49.906 c0.078-0.094,0.141-0.188,0.219-0.281H81.558c-18.75-0.016-35.984,7.531-48.25,19.594c-12.328,12.063-20.016,28.938-20,47.344 v292.844c-0.016,18.406,7.672,35.313,20,47.344C45.573,504.469,62.808,512,81.558,512h298.641c18.781,0,36.016-7.531,48.281-19.594 c12.297-12.031,20-28.938,19.984-47.344V203.469c0,0-0.125-0.156-0.328-0.313C440.37,209.813,431.323,216.156,421.073,221.719z"></path> <path d="M498.058,0c0,0-15.688,23.438-118.156,58.109C275.417,93.469,211.104,237.313,211.104,237.313 c-15.484,29.469-76.688,151.906-76.688,151.906c-16.859,31.625,14.031,50.313,32.156,17.656 c34.734-62.688,57.156-119.969,109.969-121.594c77.047-2.375,129.734-69.656,113.156-66.531c-21.813,9.5-69.906,0.719-41.578-3.656 c68-5.453,109.906-56.563,96.25-60.031c-24.109,9.281-46.594,0.469-51-2.188C513.386,138.281,498.058,0,498.058,0z"></path> </g> </g></svg>
+                                   Read
+                    </span>
+                  <h2 className="h4 md:h3 lg:h2 m-0 text-inherit mt-1">Get the latest updates</h2>
+                  <p className="fs-6 lg:fs-5 text-inherit text-center">
                     Subscribe to get our most-popular proposal eBook and more
                     top revenue content to help you send docs faster.
                   </p>
                   <form
                     onSubmit={(e) => e.preventDefault()}
-                    className="row child-cols g-1 mt-1 xl:mt-2"
+                    className="row child-cols g-1 mt-1 xl:mt-2 w-100"
                   >
                     <div>
                       <input
@@ -38,29 +49,12 @@ export default function Newsletter() {
                       </button>
                     </div>
                   </form>
-                  <p className="fs-7 text-dark dark:text-white text-opacity-70">
+                  <p className="fs-7 text-opacity-70 text-inherit">
                     Don't worry we don't spam.
                   </p>
                 </div>
               </div>
-              <div className="md:col-auto d-none md:d-block">
-                <Image
-                  className="w-250px lg:w-300px xl:w-400px d-block dark:d-none"
-                  alt="newsletter"
-                  src="/assets/images/template/newsletter.svg"
-                  width="500"
-                  height="372"
-                />
-                <Image
-                  className="w-250px lg:w-300px xl:w-400px d-none dark:d-block"
-                  alt="newsletter-dark"
-                  src="/assets/images/template/newsletter-dark.svg"
-                  width="500"
-                  height="372"
-                />
-              </div>
             </div>
-          </div>
         </div>
       </div>
     </div>
--- lexend/components/innerpages/About2.jsx
+++ lexend/components/innerpages/About2.jsx
@@ -9,35 +9,18 @@ export default function About2() {
       className="about-us section panel overflow-hidden"
       data-anime="onview: -100; opacity: [0, 1]; easing: spring(1, 80, 10, 0); duration: 450; delay: 250;"
     >
-      <div className="section-outer panel py-6 xl:py-9 bg-secondary dark:bg-gray-800">
-        <div
-          className="d-none lg:d-block"
-          data-anime="onview: -100; targets: img; scale: [0.8, 1]; opacity: [0, 1]; easing: spring(1, 80, 10, 0); duration: 450; delay: 350;"
-        >
-          <div className="position-absolute bottom-0 start-0 ms-n8 mb-6">
-            <Image
-              className="w-150px xl:w-250px d-block dark:d-none"
-              alt="walking"
-              src="/assets/images/template/walking.svg"
-              width="224"
-              height="226"
-            />
-            <Image
-              className="w-150px xl:w-250px d-none dark:d-block"
-              alt="walking-dark"
-              src="/assets/images/template/walking-dark.svg"
-              width="224"
-              height="227"
-            />
-          </div>
-        </div>
+      <div className="section-outer panel py-6 xl:py-9 border-1 border border-black">
         <div className="container max-w-xl">
           <div className="section-inner panel">
             <div
               className="panel vstack text-center"
               data-anime="onview: -100; targets: >*; translateY: [48, 0]; opacity: [0, 1]; easing: spring(1, 80, 10, 0); duration: 450; delay: anime.stagger(100, {start: 200});"
             >
-              <h2 className="h3 lg:h2 mb-4">How nexlify helps</h2>
+              <span className="fw-medium d-flex align-items-center px-2 py-1 text-primary  rounded-pill blur gap-1 m-auto border-1 border border-black shadow-sm " style={{width:'fit-content'}}>
+              <svg width={20} height={20} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM11.2274 8.56904L9.21236 10.1737C8.36695 10.8469 7.94424 11.1836 8.02675 11.5594L8.03114 11.578C8.12514 11.9515 8.66096 12.0951 9.73259 12.3823C10.3281 12.5418 10.6259 12.6216 10.7656 12.8473L10.7727 12.8592C10.9075 13.0876 10.8308 13.3737 10.6775 13.9459L10.6374 14.0954C10.2123 15.6818 9.99979 16.4749 10.4091 16.7311C10.8184 16.9872 11.4697 16.4686 12.7723 15.4314L14.7873 13.8267C15.6327 13.1535 16.0554 12.8169 15.9729 12.441L15.9686 12.4224C15.8745 12.0489 15.3387 11.9053 14.2671 11.6182C13.6716 11.4586 13.3738 11.3788 13.2341 11.1531L13.227 11.1412C13.0922 10.9128 13.1689 10.6267 13.3222 10.0546L13.3623 9.905C13.7873 8.31864 13.9999 7.52547 13.5905 7.26931C13.1812 7.01316 12.5299 7.53179 11.2274 8.56904Z" fill="#1C274C"></path> </g></svg>
+                            Power
+                    </span>
+              <h2 className="h3 lg:h2 mb-4 mt-4" >How nexlify helps</h2>
               <p className="fs-5 xl:fs-4 text-dark dark:text-white text-opacity-70">
                 Teams use nexlify to improve document workflows, insights, and
                 speed while delivering an amazing experience for their
@@ -56,11 +39,11 @@ export default function About2() {
                 easily communicate with customers through personalized documents
                 that can be created in minutes, build meaningful relationships..
               </p>
-              <div className="panel mt-6">
+              <div className="panel mt-8">
                 <div className="row child-cols-6 lg:child-cols-4 justify-center g-3 col-match">
                   {panelsData.map((panel, index) => (
                     <div key={index}>
-                      <div className="panel vstack gap-1">
+                      <div className="panel vstack gap-1 p-2 border-1 border border-black shadow-sm rounded-2">
                         <h4 className="h2 xl:h1 m-0">
                           <span data-anime={panel.animationData}>
                             {panel.value}
