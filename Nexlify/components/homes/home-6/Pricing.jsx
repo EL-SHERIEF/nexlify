46107c34946df92e9c18013a1f7f91cfd12d5663
EL-SHERIEF <entkal@outlook.com>
2025-05-04 23:27:05 +0300

    s

--- Nexlify/components/homes/home-6/Pricing.jsx
+++ Nexlify/components/homes/home-6/Pricing.jsx
@@ -111,7 +111,7 @@ export default function Pricing() {
                             </span>
                             {tier.features.map((feature, idx) => (
                               <div key={idx} className="hstack gap-1 fs-7">
-                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>                                <span>{feature}</span>
+                                 <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>                                <span>{feature}</span>
                               </div>
                             ))}
                           </div>
