9826f24ddeba16562363ebb5c136c3fa0bcf81dc
EL-SHERIEF <entkal@outlook.com>
2025-05-07 19:16:38 +0300

    s

--- Nexlify/components/homes/home-8/Blogs.jsx
+++ Nexlify/components/homes/home-8/Blogs.jsx
@@ -12,28 +12,28 @@ export default function Blogs() {
       id="blog_posts"
       className="blog-posts section panel overflow-hidden swiper-parent"
     >
-      <div className="section-outer panel py-6 lg:py-8 xl:py-10 lg:mx-2 lg:rounded-2 bg-secondary dark:bg-tertiary-700">
+      <div className="section-outer panel py-6 lg:py-8 xl:py-10 lg:mx-2">
         <div className="container">
           <div className="section-inner panel">
             <div
-              className="row child-cols-12 g-4 sm:g-6 xl:g-8"
+              className="row child-cols-12 g-2 "
               data-anime="onview: -200; targets: >*; translateY: [48, 0]; opacity: [0, 1]; easing: easeOutCubic; duration: 500; delay: anime.stagger(100, {start: 200});"
             >
               <div>
                 <div className="heading vstack gap-2 justify-center items-center text-center panel">
-                  <h2 className="title h3 xl:h2 m-0">
-                    Latest from{" "}
-                    <span className="text-tertiary dark:text-primary">
-                      our blog
-                    </span>
-                  </h2>
+                <span className="custom-tag-sm mb-2">
+               Blog & News
+              </span>
+              <h2 className="h1 fw-light m-0 text-inherit ls-1 text-dark">
+               Read Our Latest Articles
+              </h2>
                 </div>
               </div>
               <div>
-                <div className="content panel">
+                <div className="content panel mt-3">
                   <Swiper
                     className="swiper swiper-match overflow-unset lg:overflow-hidden py-2"
-                    spaceBetween={24}
+                    spaceBetween={0}
                     slidesPerView={1.1}
                     centeredSlides={true}
                     initialSlide={1}
@@ -58,7 +58,7 @@ export default function Blogs() {
                   >
                     {slidesData2.map((slide) => (
                       <SwiperSlide key={slide.id}>
-                        <article className="post type-post panel overflow-hidden vstack p-1 bg-white text-dark duration-150 hover:-translate-y-1 rounded-1-5">
+                        <article className="post type-post panel overflow-hidden vstack p-1 m-2 bg-box box-style-sm text-dark duration-150 hover:-translate-y-1 rounded-1-5">
                           <figure className="featured-image m-0 rounded ratio ratio-16x9 rounded-default uc-transition-toggle overflow-hidden">
                             <Image
                               className="media-cover image uc-transition-scale-up uc-transition-opaque"
@@ -90,7 +90,7 @@ export default function Blogs() {
                               </p>
                               <Link
                                 href={`/blog-details/${slide.id}`} // Link added directly in JSX
-                                className="uc-link text-tertiary fs-7 xl:fs-6 fw-bold hstack gap-1 sm:mt-1 xl:mt-2"
+                                className="uc-link text-primary fs-7 xl:fs-6 fw-bold hstack gap-1 sm:mt-1 xl:mt-2"
                               >
                                 <span>Read post</span>
                                 <i className="position-relative icon unicon-arrow-up-right fw-bold rtl:-rotate-90 translate-y-px" />
@@ -101,7 +101,7 @@ export default function Blogs() {
                       </SwiperSlide>
                     ))}
 
-                    <div className="swiper-pagination position-relative mt-4 spdt1 text-tertiary dark:text-primary" />
+                    <div className="swiper-pagination position-relative mt-4 spdt1 text-primary" />
                   </Swiper>
                 </div>
               </div>
